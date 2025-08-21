9d98fe9ab45ce8b980588ee3ac10634adf640996
Biswajit <paridabiswa2k@gmail.com>
2025-08-21 22:05:17 +0530

    first commit

---
+++ task-user-service/src/test/java/com/code/task_user_service/TaskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.task_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
