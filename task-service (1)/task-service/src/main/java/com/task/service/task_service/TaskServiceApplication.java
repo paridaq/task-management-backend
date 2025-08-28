351e983efb517bab488b87fd2ff07f26c7fe76b2
Biswajit <paridabiswa2k@gmail.com>
2025-08-28 14:16:15 +0530

    add task-service again

--- task-service (1)/task-service/src/main/java/com/task/service/task_service/TaskServiceApplication.java
+++ task-service (1)/task-service/src/main/java/com/task/service/task_service/TaskServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.task_service;
+package com.task.service.task_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- task-service (1)/task-service/src/test/java/com/task/service/task_service/TaskServiceApplicationTests.java
+++ task-service (1)/task-service/src/test/java/com/task/service/task_service/TaskServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.task_service;
+package com.task.service.task_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
