c5549c2b430a63cd6a6346e93030a56ef7b72012
Biswajit <paridabiswa2k@gmail.com>
2025-08-30 01:34:39 +0530

    adding some dto

--- task-service (1)/task-service/src/main/java/com/task/service/task_service/controller/TaskController.java
+++ task-service (1)/task-service/src/main/java/com/task/service/task_service/controller/TaskController.java
@@ -1,13 +1,13 @@
 package com.task.service.task_service.controller;
 
 import com.task.service.task_service.modal.Task;
+import com.task.service.task_service.modal.UserDto;
 import com.task.service.task_service.services.TaskService;
+import com.task.service.task_service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -16,9 +16,14 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
-    public ResponseEntity<Task>createTask(@RequestBody Task task, @RequestHeader("Authorization") String jwt){
+     @Autowired
+    private UserService userService;
+    @PostMapping("")
+    public ResponseEntity<Task>createTask(@RequestBody Task task, @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+        Task createdTask = taskService.createTask(task,user.getRole());
 
-        Task createdTask = taskService.createTask(task,)
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED)
 
     }
 
