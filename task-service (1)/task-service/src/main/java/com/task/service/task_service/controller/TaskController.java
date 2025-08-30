84b9518a966f1580c8a8332b694385b6fe0afc2a
Biswajit <paridabiswa2k@gmail.com>
2025-08-30 16:48:03 +0530

    addiings some controllers for task service

--- task-service (1)/task-service/src/main/java/com/task/service/task_service/controller/TaskController.java
+++ task-service (1)/task-service/src/main/java/com/task/service/task_service/controller/TaskController.java
@@ -1,13 +1,17 @@
 package com.task.service.task_service.controller;
 
 import com.task.service.task_service.modal.Task;
+import com.task.service.task_service.modal.TaskStatus;
 import com.task.service.task_service.modal.UserDto;
 import com.task.service.task_service.services.TaskService;
 import com.task.service.task_service.services.UserService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -25,6 +29,20 @@ public class TaskController {
 
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED)
 
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Task>getTaskById(@PathVariable Long id, @RequestHeader("Authorization") String jwt) throws Exception{
+        UserDto user = userService.getUserProfile(jwt);
+        Task task = taskService.getTaskById(id);
+        return new ResponseEntity<>(task,HttpStatus.CREATED);
+    }
+    @GetMapping("/user")
+    public ResponseEntity<List<Task>> getAssignedUserTask(
+            @RequestParam(required = false)TaskStatus status,
+            @RequestHeader("Authorization") String jwt) throws Exception{
+        UserDto user = userService.getUserProfile(jwt);
+        List<Task> tasks = taskService.assignedUsersTask(user.getId(),status);
+        return new ResponseEntity<>(tasks,HttpStatus.CREATED);
     }
 
 }
