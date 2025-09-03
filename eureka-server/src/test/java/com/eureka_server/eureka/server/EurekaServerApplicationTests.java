b68b855f14d727d9a13c58197cbcdfb931ce8251
Biswajit <paridabiswa2k@gmail.com>
2025-09-04 00:26:22 +0530

    adding the eureka server

---
+++ eureka-server/src/test/java/com/eureka_server/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka_server.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
